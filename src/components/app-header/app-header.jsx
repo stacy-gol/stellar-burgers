0a4fb8671a274eac93aaa3e08e79aa57fef00949
stacy-gol <anastasiya.go@dashly.io>
2024-07-04 00:30:03 +0500

    added active/inactive elems in menu

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -5,41 +5,73 @@ import {
   ProfileIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import headerStyles from "./app-header.module.css";
-import { useNavigate } from "react-router-dom";
-const constructorStyle = {
-  color: "#F2F2F3",
-};
-const buttonsStyle = {
-  color: "#8585AD",
-};
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
 
-    const onClick = () => {
-        navigate("/profile");
-    };
+  const currentPath = location.pathname;
+
+  const onClick = (path) => {
+    navigate(path);
+  };
+
   return (
     <header className={headerStyles.header}>
       <section className={headerStyles.content}>
         <nav className={headerStyles.navbar}>
-          <button className={headerStyles.navButton} type="button">
-            <BurgerIcon type="primary" />
-            <p className="text text_type_main-default" style={constructorStyle}>
+          <button
+            className={headerStyles.navButton}
+            type="button"
+            onClick={() => onClick("/")}
+          >
+            <BurgerIcon type={currentPath === "/" ? "primary" : "secondary"} />
+            <p
+              className={`${"text text_type_main-default"} ${
+                currentPath === "/"
+                  ? headerStyles.active
+                  : headerStyles.inactive
+              }`}
+            >
               Конструктор
             </p>
           </button>
-          <button className={headerStyles.navButton} type="button">
-            <ListIcon type="secondary" />
-            <p className="text text_type_main-default" style={buttonsStyle}>
+          <button
+            className={headerStyles.navButton}
+            type="button"
+            onClick={() => onClick("/feed")}
+          >
+            <ListIcon
+              type={currentPath === "/feed" ? "primary" : "secondary"}
+            />
+            <p
+              className={`${"text text_type_main-default"} ${
+                currentPath === "/feed"
+                  ? headerStyles.active
+                  : headerStyles.inactive
+              }`}
+            >
               Лента заказов
             </p>
           </button>
         </nav>
         <Logo />
-        <button className={headerStyles.loginButton} type="button" onClick={onClick}>
-          <ProfileIcon type="secondary" />
-          <p className="text text_type_main-default" style={buttonsStyle}>
+        <button
+          className={headerStyles.loginButton}
+          type="button"
+          onClick={() => onClick("/profile")}
+        >
+          <ProfileIcon
+            type={currentPath.startsWith("/profile") ? "primary" : "secondary"}
+          />
+          <p
+            className={`${"text text_type_main-default"} ${
+              currentPath.startsWith("/login" || "/profile")
+                ? headerStyles.active
+                : headerStyles.inactive
+            }`}
+          >
             Личный кабинет
           </p>
         </button>
