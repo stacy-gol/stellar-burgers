00ca3154aa714deabde0de0dc60dd6aa659d3ccc
stacy-gol <anastasiya.go@dashly.io>
2024-08-04 00:01:14 +0500

    finished profile feed layout

--- src/pages/profile-feed/profile-feed.tsx
+++ src/pages/profile-feed/profile-feed.tsx
@@ -9,10 +9,12 @@ import { selectProfileOrders } from "../../services/middleware/orderFeed/selecto
 import { OrderDetail } from "../../services/types";
 import profileFeedStyles from "./profile-feed.module.css";
 import { getCookie } from "../../utils/cookies";
-
+import { NavLink, useNavigate } from "react-router-dom";
+import { logoutUser } from "../../services/authSlice";
 
 export function ProfileFeed() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const profileOrders = useSelector(selectProfileOrders);
 
@@ -30,10 +32,69 @@ export function ProfileFeed() {
     };
   }, [dispatch]);
 
+  const handleLogout = async () => {
+    //@ts-ignore
+    const result = await dispatch(logoutUser());
+    if (logoutUser.fulfilled.match(result)) {
+      navigate("/");
+    } else {
+    }
+  };
+
   return (
-    <>
+    <div className={profileFeedStyles.layout}>
+      <div className={`${profileFeedStyles.navBar} mr-15`}>
+        <div className={profileFeedStyles.navElement}>
+          <NavLink
+            end
+            to={`/profile`}
+            className={({ isActive }) =>
+              isActive
+                ? `${profileFeedStyles.activeNavElement} text text_type_main-medium`
+                : `${profileFeedStyles.navElement} text text_type_main-medium`
+            }
+          >
+            Профиль
+          </NavLink>
+        </div>
+        <div className={profileFeedStyles.navElement}>
+          <NavLink
+            end
+            to={`/profile/orders`}
+            className={({ isActive }) =>
+              isActive
+                ? `${profileFeedStyles.activeNavElement} text text_type_main-medium`
+                : `${profileFeedStyles.navElement} text text_type_main-medium`
+            }
+          >
+            История заказов
+          </NavLink>
+        </div>
+        <div className={profileFeedStyles.navElement}>
+          <NavLink
+            to={`/`}
+            className={
+              profileFeedStyles.navElement + " text text_type_main-medium"
+            }
+            onClick={handleLogout}
+          >
+            Выход
+          </NavLink>
+          {/* <Button
+              type="primary"
+              size="medium"
+              htmlType="button" 
+              onClick={handleLogout}
+            >
+              Выход
+            </Button> */}
+        </div>
+        <p className="text text_type_main-default text_color_inactive mt-20">
+        В этом разделе вы можете <br></br> изменить свои персональные данные
+      </p>
+      </div>
       <div className={profileFeedStyles.content}>
-        <h1 className="text text_type_main-large mt-10 mb-5">Лента заказов</h1>
+        <h1 className="text text_type_main-large mb-5">Лента заказов</h1>
         <div className={profileFeedStyles.container}>
           <div className={profileFeedStyles.contentleft}>
             <div className={profileFeedStyles.orderContainer}>
@@ -44,6 +105,6 @@ export function ProfileFeed() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
